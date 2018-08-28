0e2306138263f0eaad52b1d09840357c0fac31fe
Alexandre Pedrecal <pedrecalc@gmail.com>
2018-08-27 23:05:25 -0300

    Added Router, 404 Page, And form submiting Event. Minor changes on Header and helpers

---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <h2>
+      Achou que tinha uma page aqui? Achou errado otário!
+      <span role="img" aria-label="Dedo do meio">
+        🖕🏽
+      </span>
+    </h2>
+  </div>
+);
+
+export default NotFound;
